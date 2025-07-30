4332253e215c4d4a027dc68e3e63b9b4fbdc170a
anshadh-pixel <ansarmon0000@gmail.com>
2025-07-30 23:33:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(Server running on port ${PORT}));
+server.listen(PORT, () => console.log('Server running on port ${PORT}'));
